bf55dd418546a70cea8ab68396f6f122d6ef01ec
MaksymBalaniuk <balanyuk.m2403@gmail.com>
2025-10-22 17:34:39 +0300

    Initial commit

---
+++ src/main/java/com/cherrymarket/CherryMarketAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cherrymarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherryMarketAuthenticationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CherryMarketAuthenticationServiceApplication.class, args);
+    }
+
+}
