b82eefcbed1942741fb65da2bab7c3da61907aa7
MaksymBalaniuk <balanyuk.m2403@gmail.com>
2025-10-26 18:41:06 +0200

    Added ValidationConfiguration

---
+++ src/main/java/com/cherrymarket/configuration/validation/ValidationConfiguration.java
@@ -0,0 +1,22 @@
+package com.cherrymarket.configuration.validation;
+
+import com.cherrymarket.configuration.localization.YamlMessageSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+@RequiredArgsConstructor
+public class ValidationConfiguration {
+
+    private final YamlMessageSource messageSource;
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource);
+        return bean;
+    }
+
+}
